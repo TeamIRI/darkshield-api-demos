18c1df1fc2720188c9d851e539d81029d9e8e23c
Dmitry Kulakov <dimakula@gmail.com>
2021-02-10 14:06:39 -0500

    Updated s3 demo so that it can use an s3 url instead of a bucket name as well.

--- s3/README.md
+++ s3/README.md
@@ -5,11 +5,30 @@ mask files located on an S3 bucket. To run, the *plankton* web services API
 must be hosted on *http://localhost:8080* and must have the *darkshield* and 
 *darkshield-files* plugins installed.
 
-The code will iterate over all objects inside of the bucket and download them
-before sending them to the API. The original and masked files are loaded
-fully in memory, for larger files that cannot be fit into memory the code needs
-to be modified to allow for a streaming solution (see comments in *main.py* for
-more information).
+To install the additional dependencies, execute *pip install -r requirements.txt* 
+(make sure your virtual environment is activated, or your dependencies will 
+be installed globally).
+
+The setup script will also download the NER and sentence detection models from 
+the [OpenNLP website](http://opennlp.sourceforge.net/models-1.5/) if not present 
+in the parent directory. If you are having trouble downloading the models, place 
+the *en-ner-person.bin*, *en-sent.bin*, and *en-token.bin* files into the 
+parent directory.
+
+The program will take in a bucket name or an s3 url (starting with 's3://') and 
+iterate over all objects found there, sending each file to the API. The s3 url can 
+contain a prefix, which means only files under a certain prefix will be searched 
+and masked. A single file can also be specified using its s3 url.
+
+The original and masked files are loaded fully in memory, for larger files that
+cannot be fit into memory the code needs to be modified to allow for a streaming 
+solution (see comments in *main.py* for more information).
+
+The masked files will be placed under the *masked* key in the root of the original 
+bucket.
+
+The *results.json* for each masked file will be placed inside of the *results*
+folder.
 
 The demo is best performed with test files obtained from the *json-xml* folder,
 the *setup.py* code was copied from there.
@@ -24,22 +43,7 @@ using format-preserving encryption.
 using format-specific json/xml paths (all names can be found in the 'name' key/
 tag, regardless of nesting).
 
-To execute, run *python main.py bucket_name*. The script will use the *default*
-profile in your AWS credentials file in order to access the bucket. See the
-[boto3 quickstart guide](https://boto3.amazonaws.com/v1/documentation/api/latest/guide/quickstart.html#configuration)
+To execute, run *python main.py bucket_name_or_url*. The script will use the *default*
+profile in your AWS credentials file in order to access the bucket unless the
+*--profile* flag is included. See the [boto3 quickstart guide](https://boto3.amazonaws.com/v1/documentation/api/latest/guide/quickstart.html#configuration)
 for more information on how to configure this file.
-
-To install the additional dependencies, execute *pip install -r requirements.txt* 
-(make sure your virtual environment is activated, or your dependencies will 
-be installed globally).
-
-The masked files will be placed under the *masked* key in the original bucket.
-
-The *results.json* for each masked file will be placed inside of the *results*
-folder.
-
-The setup script will also download the NER and sentence detection models from 
-the [OpenNLP website](http://opennlp.sourceforge.net/models-1.5/) if not present 
-in the parent directory. If you are having trouble downloading the models, place 
-the *en-ner-person.bin*, *en-sent.bin*, and *en-token.bin* files into the 
-parent directory.
--- s3/main.py
+++ s3/main.py
@@ -1,3 +1,4 @@
+import argparse
 import boto3
 import io
 import json
@@ -17,53 +18,74 @@ from streaming_form_data import StreamingFormDataParser
 from streaming_form_data.targets import ValueTarget, FileTarget, NullTarget
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print('Usage: python main.py bucket_name')
-    else:   
-        logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
-        bucket_name = sys.argv[1]
-        s3 = boto3.resource('s3')
-        bucket = s3.Bucket(bucket_name)
-        try:
-            setup()
-            url = 'http://localhost:8080/api/darkshield/files/fileSearchContext.mask'
-            context = json.dumps({
-                "fileSearchContextName": file_search_context_name,
-                "fileMaskContextName": file_mask_context_name
-            })
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
+    parser = argparse.ArgumentParser(description='Demo for S3 bucket search/masking.')
+    parser.add_argument('bucket_name_or_url', type=str, help="The name of the bucket, or the s3 url of the object (starting with 's3://').")
+    parser.add_argument('-p', '--profile', metavar='name', type=str, 
+                        help='The name of AWS profile to use for the connection (otherwise the default is used).')
 
-            for obj in bucket.objects.all():
-                file_name = obj.key
-                obj.load() # Load the metadata for this object.
-                content_type = obj.meta.data.get('ContentType', 'application/octet-stream')
-                # Skip prefix folders or files that were already masked.
-                if content_type.startswith('application/x-directory') or file_name.startswith('masked'):
-                    continue
-                # For larger files, a streaming solution should be used so that the file isn't loaded
-                # fully in memory before being sent to the API.
-                f = obj.get()['Body'].read()
-                files = {'file': (file_name, io.BytesIO(f), content_type), 'context': context}
-                logging.info(f"POST: sending '{file_name}' to {url}")
-                with requests.post(url, files=files, stream=True) as r:
-                    if r.status_code >= 300:
-                        raise Exception(f"Failed with status {r.status_code}:\n\n{r.json()}")
+    args = parser.parse_args()
+    if args.profile:
+        session = boto3.Session(profile_name=args.profile)
+    else:
+        session = boto3.Session()
 
-                    folder_name = f"results/{file_name.replace('.', '_')}"
-                    os.makedirs(folder_name, exist_ok=True)
-                    logging.info(f"Extracting 'results.json' for '{file_name}' into '{folder_name}' folder.")
-                    parser = StreamingFormDataParser(headers=r.headers)
-                    # For larger files, a temporary file needs to be created and then streamed to the
-                    # S3 bucket.
-                    masked_file = ValueTarget()
-                    parser.register('file', masked_file)
-                    parser.register('results', FileTarget(f'{folder_name}/results.json'))
-                    for chunk in r.iter_content():
-                        parser.data_received(chunk)
+    s3 = session.resource('s3')
+    bucket_name = args.bucket_name_or_url
+    prefix = ''
+    if bucket_name.startswith('s3://'):
+        split = bucket_name[5:].split('/', 1)
+        if len(split) == 2:
+            bucket_name, prefix = split
+        else:
+            bucket_name = split[0]
+    
+    bucket = s3.Bucket(bucket_name)
+    try:
+        setup()
+        url = 'http://localhost:8080/api/darkshield/files/fileSearchContext.mask'
+        context = json.dumps({
+            "fileSearchContextName": file_search_context_name,
+            "fileMaskContextName": file_mask_context_name
+        })
+        if prefix:
+            logging.info(f"Filtering on prefix '{prefix}'...")
+            objects = bucket.objects.filter(Prefix=prefix)
+        else:
+            logging.info('Extracting all objects from bucket...')
+            objects = bucket.objects.all()
+        
+        for obj in objects:
+            file_name = obj.key
+            obj.load() # Load the metadata for this object.
+            content_type = obj.meta.data.get('ContentType', 'application/octet-stream')
+            # Skip prefix folders or files that were already masked.
+            if content_type.startswith('application/x-directory') or file_name.startswith('masked'):
+                continue
+            # For larger files, a streaming solution should be used so that the file isn't loaded
+            # fully in memory before being sent to the API.
+            f = obj.get()['Body'].read()
+            files = {'file': (file_name, io.BytesIO(f), content_type), 'context': context}
+            logging.info(f"POST: sending '{file_name}' to {url}")
+            with requests.post(url, files=files, stream=True) as r:
+                if r.status_code >= 300:
+                    raise Exception(f"Failed with status {r.status_code}:\n\n{r.json()}")
 
-                    key = f'masked/{file_name}'
-                    logging.info(f"Putting masked file in '{key}'.")
-                    value = masked_file.value    
-                    bucket.put_object(Key=key,
-                                      Body=masked_file.value)
-        finally:
-            teardown()
+                folder_name = f"results/{file_name.replace('.', '_')}"
+                os.makedirs(folder_name, exist_ok=True)
+                logging.info(f"Extracting 'results.json' for '{file_name}' into '{folder_name}' folder.")
+                parser = StreamingFormDataParser(headers=r.headers)
+                # For larger files, a temporary file needs to be created and then streamed to the
+                # S3 bucket.
+                masked_file = ValueTarget()
+                parser.register('file', masked_file)
+                parser.register('results', FileTarget(f'{folder_name}/results.json'))
+                for chunk in r.iter_content():
+                    parser.data_received(chunk)
+
+                key = f'masked/{file_name}'
+                logging.info(f"Putting masked file in '{key}'.")
+                value = masked_file.value    
+                bucket.put_object(Key=key, Body=masked_file.value)
+    finally:
+        teardown()
