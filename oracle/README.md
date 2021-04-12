23da94270def1f0bd11d12b6fe56eac8a836d66d
Devon Kozenieski <devonk@iri.com>
2021-04-12 12:15:53 -0400

    Started Oracle demo

---
+++ oracle/README.md
@@ -0,0 +1,29 @@
+# DarkShield Files API: Oracle Search/Masking
+
+This example demonstrates the use of the *darkshield-files* API to search and mask Oracle database values. To run, the *plankton* web services API must be hosted on 
+*http://localhost:8080* and must have the *darkshield* and *darkshield-files* plugins 
+installed.
+
+You must have a Oracle database installed and hosted on *http://localhost:1521*. The Oracle client libraries must be 
+installed as well. Python 3 should be used.
+
+To install the additional dependencies, execute *pip install -r requirements.txt* 
+(make sure your virtual environment is activated, or your dependencies will 
+be installed globally).
+
+The script will search all values in the database and table specified, and mask based on predefined rules.
+
+The example will find and mask the following:
+
+1. Social Security Numbers (SsnMatcher): Found using a regular expression. The first 5 digits of the Social Security Number,
+excluding the dashes, will be redacted with the '*' character.
+2. Names (NameMatcher): Found using a Named Entity Recognition (NER) model. Will encrypt the names with 
+format-preserving alphanumeric encryption.
+
+To execute, run *python main.py [host] [username] [password] [dsn] [database] [table]*.
+
+
+The setup script will also download the NER and sentence detection models from the
+[OpenNLP website](http://opennlp.sourceforge.net/models-1.5/) if not present in the
+directory. If you are having trouble downloading the models, place the *en-ner-person.bin*
+and *en-sent.bin* files into this directory.
---
+++ oracle/main.py
@@ -0,0 +1,111 @@
+import io
+import json
+import logging
+import base64
+import os
+import cx_Oracle
+import re
+import requests
+import sys
+
+# Append parent directory to PYTHON_PATH so we can import utils.py
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+from setup import setup, teardown, file_mask_context_name, file_search_context_name
+from streaming_form_data import StreamingFormDataParser
+from streaming_form_data.targets import ValueTarget, FileTarget,NullTarget
+
+if __name__ == "__main__":
+# args: [host] [username] [password] [database] [table]
+        if len(sys.argv) < 7:
+            print("Usage: python main.py [host] [username] [password] [dsn] [database] [table]")
+            print("Please provide all arguments.")
+            exit(0)
+        logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
+        url = 'http://localhost:8080/api/darkshield/searchContext.mask'
+        urlfile = 'http://localhost:8080/api/darkshield/files/fileSearchContext.mask'
+        contextfile = json.dumps({
+                "fileSearchContextName": file_search_context_name,
+                "fileMaskContextName": file_mask_context_name
+            })
+        try:
+            setup()
+            host=sys.argv[1]
+            user=sys.argv[2]
+            password=sys.argv[3]
+            dsn=sys.argv[4]
+            connection =cx_Oracle.connect(
+           user=user,
+           password=password,
+           dsn=dsn
+           )
+           
+            cursor=connection.cursor()
+            database = sys.argv[5]
+            table = sys.argv[6]
+            # get all the data from specified table
+            cursor.execute("SELECT * FROM {}".format(table))
+            result=cursor.fetchall()
+            logging.info("Getting data from database '{}' table '{}'.".format(database,table))
+            #myresult = cursor.fetchall()
+            s=requests.Session()
+            count=0
+            field_names = [i[0] for i in cursor.description]
+            data_types = [i[1] for i in cursor.description]
+            #print(field_names)
+            #print(data_types)
+           # exit(0)
+            # loop through rows, then values
+            for row in result:
+                # log progess every 25 rows
+                if count % 25 == 0:
+                    logging.info("Masking row {} of table '{}'.".format(count+1,table))
+                firstval=''
+                firstNum=0
+                colNum=0
+                for value in row:
+                    # if this is a blob type, handle differently
+                    # this covers tinyblob to longblob data types;
+                    # ensure this is a file and not floating text.
+                    if data_types[colNum] == cx_Oracle.DB_TYPE_BLOB:
+                        #try:
+                        value2=value.read()
+                        files = {'file': io.BytesIO(value2), 'context': contextfile}
+                        with s.post(urlfile, files=files, stream=True) as r:
+                            if r.status_code >= 300:
+                                raise Exception(f"Failed with status {r.status_code}:\n\n{r.json()}")
+                            fileval=ValueTarget()
+                            parser = StreamingFormDataParser(headers=r.headers)
+                            parser.register('file', fileval)
+                            #parser.register('results', FileTarget("resultsblob{}_{}.json".format(count,colNum)))
+                            for chunk in r.iter_content():
+                                parser.data_received(chunk)
+                            query = "UPDATE {} SET {} = :1 WHERE {} = :2".format(table,field_names[colNum],field_names[firstNum])
+                            if len(fileval.value) > 0:
+                                args = (fileval.value,firstval)
+                                cursor.execute(query,args)
+                                connection.commit()
+                    else:
+                    # try to get a value to use as a reference for update statements. Hopefully a unique row id.
+                        if firstval == '':
+                            firstval = value
+                            firstnum =colNum
+                        # unstructured text or basic values; handle using base DarkShield API
+                        print(value)
+                        with s.post(url, json={"searchContextName": "SearchContext","maskContextName": "MaskContext","text": "{}".format(value)}) as r:
+                            if r.status_code >= 300:
+                                raise Exception(f"Failed with status {r.status_code}:\n\n{r.json()}")
+                            query = "UPDATE {} SET {} = :1 WHERE {} = :2".format(table,field_names[colNum],field_names[firstNum])
+                            args=(r.json()['maskedText'], firstval)
+                            cursor.execute(query,args)
+                            connection.commit()
+                    colNum+=1
+                count+=1
+                 # uncomment to end after certain number of rows
+                #if count>=rows:
+                 #   return 1
+            logging.info("Completed masking {} rows of table '{}'.".format(count,table))
+        finally:
+            teardown()
