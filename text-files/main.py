08a7fba9eacbc72307aebbbdf6b0bedb9e8395c8
Dmitry Kulakov <dimakula@gmail.com>
2021-05-26 15:09:43 -0400

    text-files/main.py: fixed old code.

--- text-files/main.py
+++ text-files/main.py
@@ -27,19 +27,20 @@ if __name__ == "__main__":
             "fileMaskContextName": file_mask_context_name
         })
 
-        process_files = [('example.txt', 'text-masked')]
+        process_files = [('example.txt', 'text/plain', 'text-masked')]
 
-        for file_name, masked_folder in process_files:
+        for file_name, media_type, masked_folder in process_files:
             with open(file_name, 'rb') as f:
                 encoder = MultipartEncoder(fields={
                     'context': ('context', context, 'application/json'),
-                    'file': (file_name, f, 'text/plain')
+                    'file': (file_name, f, media_type)
                 })
-            os.makedirs(masked_folder, exist_ok=True)
-            logging.info(f"POST: sending '{file_name}' to {url}")
-            with session.post(url, files=files, stream=True) as r:
-                if r.status_code >= 300:
-                    raise Exception(f"Failed with status {r.status_code}:\n\n{r.json()}")
+                os.makedirs(masked_folder, exist_ok=True)
+                logging.info(f"POST: sending '{file_name}' to {url}")
+                with session.post(url, data=encoder, stream=True,
+                                    headers={'Content-Type': encoder.content_type}) as r:
+                    if r.status_code >= 300:
+                        raise Exception(f"Failed with status {r.status_code}:\n\n{r.json()}")
                     logging.info(f"Extracting '{file_name}' and 'results.json' into {masked_folder}.")
                     parser = StreamingFormDataParser(headers=r.headers)
                     parser.register('file', FileTarget(f'{masked_folder}/{file_name}'))
