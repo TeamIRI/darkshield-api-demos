72cb29984b12eb50506e6c4355d73e7dbaaa1e30
Dmitry Kulakov <dimakula@gmail.com>
2021-08-23 09:32:13 -0400

    Added kafka example.

---
+++ kafka/README.md
@@ -0,0 +1,42 @@
+# DarkShield Files API: Kafka Message Search/Masking
+
+This example demonstrates the use of the *darkshield-files* API to search and
+mask json messages on a Kafka topic, and output masked messages and results
+to separate topics. To run, the *plankton* web services API must be hosted on
+*http://localhost:8080* and must have the *darkshield* and *darkshield-files*
+plugins installed.
+
+To install the additional dependencies, execute *pip install -r requirements.txt*
+(make sure your virtual environment is activated, or your dependencies will
+be installed globally).
+
+Follow the [quickstart](https://kafka.apache.org/quickstart) guides to set up
+a basic Kafka environment.
+
+This demo uses three topics which have to be created:
+* darkshield-demo
+* darkshield-demo-masked
+* darkshield-demo-results
+
+You may wish to configure the retention time of the messages in the topics to
+clean up the inboxes between demo runs.
+
+A producer thread will be started to send the contents of *message.json* to the
+*darkshield-demo* topic every 3 seconds. A consumer thread will pick up the
+messages and send them to the API for search/masking. The masked message will
+then be sent to the *darkshield-demo-masked* topic, while the *results.json*
+audit log for the message will be sent to the *darkshield-demo-results* topic.
+
+The example will find and mask the following:
+
+1. Emails (EmailMatcher): Found using a regular expression and masked using a hashing
+function.
+2. Phone Numbers (PhoneMatcher): Found using a regular expression and masked using
+format-preserving encryption.
+3. Names (NameMatcher): Found using a Named Entity Recognition (NER) model AND using
+format-specific json paths (all names can be found in the 'name' key/tag, regardless
+of nesting).
+
+To execute, run *python main.py*.
+
+The results will be placed inside of the *text-masked* directory.
---
+++ kafka/main.py
@@ -0,0 +1,100 @@
+import io
+import json
+import logging
+import os
+import re
+import requests
+import signal
+import sys
+import time
+
+# Append parent directory to PYTHON_PATH so we can import utils.py
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+from kafka import KafkaConsumer, KafkaProducer
+from multiprocessing import Process
+from requests_toolbelt import MultipartEncoder
+from setup import setup, teardown, file_mask_context_name, file_search_context_name
+from streaming_form_data import StreamingFormDataParser
+from streaming_form_data.targets import ValueTarget
+
+
+bootstrap_servers = 'localhost:9092'
+source_topic = 'darkshield-demo'
+masked_topic = 'darkshield-demo-masked'
+result_topic = 'darkshield-demo-results'
+
+
+def consume_events(session):
+    logging.info('Started consumer.')
+    url = 'http://localhost:8080/api/darkshield/files/fileSearchContext.mask'
+    context = json.dumps({
+        "fileSearchContextName": file_search_context_name,
+        "fileMaskContextName": file_mask_context_name
+    })
+
+    consumer = KafkaConsumer(source_topic, bootstrap_servers=bootstrap_servers)
+    producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
+    for message in consumer:
+        logging.info('Consumer: Read message.')
+        logging.info("Consumer: sending message to %s", url)
+        files = {
+            'context': context,
+            'file': ('message.json', io.BytesIO(message.value), 'application/json')
+        }
+        with session.post(url, files=files) as r:
+            if r.status_code >= 300:
+                raise Exception(f"Failed with status {r.status_code}:\n\n{r.json()}")
+
+            parser = StreamingFormDataParser(headers=r.headers)
+            masked = ValueTarget()
+            results = ValueTarget()
+            parser.register('file', masked)
+            parser.register('results', results)
+            for chunk in r.iter_content(4096):
+                parser.data_received(chunk)
+
+            logging.info('Consumer: Sending masked data to "%s"...', masked_topic)
+            producer.send(masked_topic, masked.value)
+            logging.info('Consumer: Sending results data to "%s"...', result_topic)
+            producer.send(result_topic, results.value)
+            logging.info('Consumer: Messages sent.')
+
+
+def produce_events():
+    logging.info('Started producer.')
+    producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
+    with open('message.json', 'rb') as f:
+        message = f.read()
+
+    while True:
+        producer.send(source_topic, message)
+        logging.info('Producer: Sent message.')
+        time.sleep(3)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(format='[%(levelname)s]: %(message)s', level=logging.INFO)
+
+    session=requests.Session()
+    try:
+        setup(session)
+        consumer = Process(target=consume_events, args=[session], name="ConsumerThread")
+        consumer.start()
+
+        producer = Process(target=produce_events, name='ProducerThread')
+        producer.start()
+
+        # Graceful handling of ctrl-C termination.
+        def terminate_processes(*args):
+            consumer.terminate()
+            producer.terminate()
+
+        signal.signal(signal.SIGINT, terminate_processes)
+        signal.signal(signal.SIGTERM, terminate_processes)
+
+        producer.join()  # Freeze thread until the producer process is terminated.
+    finally:
+        teardown(session)
