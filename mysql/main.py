640d4d0063f58716f43a352e8a811e58ee728791
Devon Kozenieski <devonk@iri.com>
2021-09-07 16:50:28 -0400

    Issue with MySQL demo resolved.

--- mysql/main.py
+++ mysql/main.py
@@ -1,10 +1,8 @@
 import io
 import json
 import logging
-import base64
 import os
 import mysql.connector
-import re
 import requests
 import sys
 
@@ -15,99 +13,92 @@ sys.path.append(parent_dir)
 
 from setup import setup, teardown, file_mask_context_name, file_search_context_name
 from streaming_form_data import StreamingFormDataParser
-from streaming_form_data.targets import ValueTarget, FileTarget,NullTarget
+from streaming_form_data.targets import ValueTarget
 
 if __name__ == "__main__":
-# args: [host] [username] [password] [database] [table]
-        if len(sys.argv) < 6:
-            print("Usage: python main.py [host] [username] [password] [database] [table]")
-            print("Please provide all arguments.")
-            exit(0)
-        logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
-        s=requests.Session()
-        url = 'http://localhost:8080/api/darkshield/searchContext.mask'
-        urlfile = 'http://localhost:8080/api/darkshield/files/fileSearchContext.mask'
-        contextfile = json.dumps({
-                "fileSearchContextName": file_search_context_name,
-                "fileMaskContextName": file_mask_context_name
-            })
-        try:
-            setup(s)
-            host=sys.argv[1]
-            user=sys.argv[2]
-            password=sys.argv[3]
-            database=sys.argv[4]
-            mydb = mysql.connector.connect(
+    # args: [host] [username] [password] [database] [table]
+    if len(sys.argv) < 6:
+        print("Usage: python main.py [host] [username] [password] [database] [table]")
+        print("Please provide all arguments.")
+        exit(0)
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
+    s = requests.Session()
+    url = 'http://localhost:8080/api/darkshield/searchContext.mask'
+    urlfile = 'http://localhost:8080/api/darkshield/files/fileSearchContext.mask'
+    contextfile = json.dumps({
+            "fileSearchContextName": file_search_context_name,
+            "fileMaskContextName": file_mask_context_name
+        })
+    try:
+        setup(s)
+        host = sys.argv[1]
+        user = sys.argv[2]
+        password = sys.argv[3]
+        database = sys.argv[4]
+        mydb = mysql.connector.connect(
             host=host,
             user=user,
             password=password,
             database=database
-          )
-            mycursor = mydb.cursor()
-            table=sys.argv[5]
-            # get all the data from specified table
-            mycursor.execute("SELECT * FROM {}".format(table))
-            logging.info("Getting data from database '{}' table '{}'.".format(database,table))
-            myresult = mycursor.fetchall()
-            count=0
-            field_names = [i[0] for i in mycursor.description]
-            data_types = [i[1] for i in mycursor.description]
-            # loop through rows, then values
-            for row in myresult:
-                # log progess every 25 rows
-                if count % 25 == 0:
-                    logging.info("Masking row {} of table '{}'.".format(count+1,table))
-                firstval=''
-                firstNum=0
-                colNum=0
-                for value in row:
-                    # if this is a blob type, handle differently
-                    # this covers tinyblob to longblob data types;
-                    # ensure this is a file and not floating text.
-                    if type(value) == bytes:
-                        if data_types[colNum] >= 249 and data_types[colNum] <= 252:
-                            #try:
-                            files = {'file': io.BytesIO(value), 'context': contextfile}
-                            with s.post(urlfile, files=files, stream=True) as r:
-                                if r.status_code >= 300:
-                                    raise Exception(f"Failed with status {r.status_code}:\n\n{r.json()}")
-                                fileval=ValueTarget()
-                                parser = StreamingFormDataParser(headers=r.headers)
-                                parser.register('file', fileval)
-                                    #parser.register('results', FileTarget(f'resultsblob.json'))
-                                for chunk in r.iter_content():
-                                    parser.data_received(chunk)
-                                query = "UPDATE {} SET {} = %s WHERE {} = %s".format(table,field_names[colNum],field_names[firstNum])
-                                if len(fileval.value) > 0:
-                                    args = (fileval.value,firstval)
-                                    mycursor.execute(query,args)
-                                    mydb.commit()
-                    else:
-                    # try to get a value to use as a reference for update statements. Hopefully a unique row id.
-                        if firstval == '':
-                            firstval = value
-                            firstnum =colNum
-                        # unstructured text or basic values; handle using base DarkShield API
-                        
-                        with s.post(url, json={"searchContextName": "SearchContext","maskContextName": "MaskContext","text": "{}".format(value)}) as r:
+        )
+        mycursor = mydb.cursor()
+        table = sys.argv[5]
+        # get all the data from specified table
+        mycursor.execute("SELECT * FROM {}".format(table))
+        logging.info("Getting data from database '{}' table '{}'.".format(database, table))
+        myresult = mycursor.fetchall()
+        count = 0
+        field_names = [i[0] for i in mycursor.description]
+        data_types = [i[1] for i in mycursor.description]
+        # loop through rows, then values
+        for row in myresult:
+            # log progess every 25 rows
+            if count % 25 == 0:
+                logging.info("Masking row {} of table '{}'.".format(count+1, table))
+            firstval = ''
+            firstNum = 0
+            colNum = 0
+            for value in row:
+                # if this is a blob type, handle differently
+                # this covers tinyblob to longblob data types;
+                # ensure this is a file and not floating text.
+                if type(value) == bytes:
+                    if 249 <= data_types[colNum] <= 252:
+                        files = {'file': io.BytesIO(value), 'context': contextfile}
+                        with s.post(urlfile, files=files, stream=True) as r:
                             if r.status_code >= 300:
                                 raise Exception(f"Failed with status {r.status_code}:\n\n{r.json()}")
-                            
-                            fileval=ValueTarget()
+                            fileval = ValueTarget()
                             parser = StreamingFormDataParser(headers=r.headers)
                             parser.register('file', fileval)
-                            #parser.register('results', FileTarget(f'resultsblob.json'))
-                            for chunk in r.iter_content(4096):
+                            for chunk in r.iter_content():
                                 parser.data_received(chunk)
-                            query = "UPDATE {} SET {} = %s WHERE {} = %s".format(table,field_names[colNum],field_names[firstNum])
-                            args=(r.json()['maskedText'], firstval)
-                            mycursor.execute(query,args)
-                            mydb.commit()
-                    colNum+=1
-                count+=1
-                 # uncomment to end after certain number of rows
-                #if count>=rows:
-                 #   return 1
-            logging.info("Completed masking {} rows of table '{}'.".format(count,table))
-        finally:
-            teardown(s)
+                            query = "UPDATE {} SET {} = %s WHERE {} = %s".format(table, field_names[colNum],
+                                                                                 field_names[firstNum])
+                            if len(fileval.value) > 0:
+                                args = (fileval.value, firstval)
+                                mycursor.execute(query, args)
+                                mydb.commit()
+                else:
+                    # try to get a value to use as a reference for update statements. Hopefully a unique row id.
+                    if firstval == '':
+                        firstval = value
+                        firstnum = colNum
+                    # unstructured text or basic values; handle using base DarkShield API
+                    with s.post(url, json={"searchContextName": "SearchContext", "maskContextName":
+                                "MaskContext", "text": "{}".format(value)}) as r:
+                        if r.status_code >= 300:
+                            raise Exception(f"Failed with status {r.status_code}:\n\n{r.json()}")
+                        query = "UPDATE {} SET {} = %s WHERE {} = %s".format(table, field_names[colNum],
+                                                                             field_names[firstNum])
+                        args = (r.json()['maskedText'], firstval)
+                        mycursor.execute(query, args)
+                        mydb.commit()
+                colNum += 1
+            count += 1
+            # uncomment to end after certain number of rows
+            # if count>=rows:
+            #   return 1
+        logging.info("Completed masking {} rows of table '{}'.".format(count, table))
+    finally:
+        teardown(s)
