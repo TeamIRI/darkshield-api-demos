75d1b0b46b7afab656d5b204f5e67acbde2a3cbe
Devon Kozenieski <devonk@iri.com>
2021-12-29 12:19:17 -0500

    Add demo for masking images of checks using user-specified bounding boxes.

---
+++ pdf-image/check/setup.py
@@ -0,0 +1,83 @@
+import pathlib
+
+import utils
+
+search_context_name = "SearchContext"
+mask_context_name = "MaskContext"
+file_search_context_name = "FileSearchContext"
+file_mask_context_name = "FileMaskContext"
+
+
+def setup(session):
+    model_url = utils.download_model('en-ner-person.bin', session)
+    sent_url = utils.download_model('en-sent.bin', session)
+    token_url = utils.download_model('en-token.bin', session)
+    search_context = {
+        "name": search_context_name,
+        "matchers": [
+            {
+                "name": "noMatcher",
+                "type": "pattern",
+                "pattern": r"sgosgfnnfsgkjngfdnjkfgdkfgdk"
+            }
+        ]
+    }
+
+    mask_context = {
+        "name": mask_context_name,
+        "rules": [
+            {
+                "name": "FpeRule",
+                "type": "cosort",
+                "expression": r"enc_fp_aes256_alphanum(${INPUT})"
+            }
+        ],
+        "ruleMatchers": [
+            {
+                "name": "FpeRuleMatcher",
+                "type": "name",
+                "rule": "FpeRule",
+                "pattern": ".*"
+            }
+        ]
+    }
+
+    file_search_context = {
+        "name": file_search_context_name,
+        "matchers": [
+            {
+                "name": search_context_name,
+                "type": "searchContext"
+            }
+        ]
+    }
+
+    file_mask_context = {
+        "name": file_mask_context_name,
+        "rules": [
+            {
+                "name": mask_context_name,
+                "type": "maskContext"
+            }
+        ],
+ "configs": {
+    "image": {
+      "boundingBoxes": [
+      "0.018867924528301886,0.019762845849802372,0.20411663807890223,0.21739130434782608", "0.06689536878216124,0.8260869565217391,0.2538593481989708,0.8932806324110671", "0.3018867924528302,0.8221343873517787,0.5094339622641509,0.9011857707509882",
+      "0.10634648370497427,0.2608695652173913,0.3979416809605489,0.35968379446640314", "0.060034305317324184,0.6837944664031621,0.21269296740994853,0.7747035573122529", "0.614065180102916,0.6719367588932806,0.8610634648370498,0.7905138339920948"
+      ]
+    }
+  }
+    }
+
+    utils.create_context("searchContext", search_context, session)
+    utils.create_context("maskContext", mask_context, session)
+    utils.create_context("files/fileSearchContext", file_search_context, session)
+    utils.create_context("files/fileMaskContext", file_mask_context, session)
+
+
+def teardown(session):
+    utils.destroy_context("searchContext", search_context_name, session)
+    utils.destroy_context("maskContext", mask_context_name, session)
+    utils.destroy_context("files/fileSearchContext", file_search_context_name, session)
+    utils.destroy_context("files/fileMaskContext", file_mask_context_name, session)
