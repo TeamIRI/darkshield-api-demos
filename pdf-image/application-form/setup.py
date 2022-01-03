c6533e67d7397d388dfd4fc12ac2edbda68d4680
Devon Kozenieski <devonk@iri.com>
2022-01-03 16:13:57 -0500

    Add PDF demo for an application with user input forms.

---
+++ pdf-image/application-form/setup.py
@@ -0,0 +1,101 @@
+import pathlib
+
+import utils
+
+search_context_name = "SearchContext"
+mask_context_name = "MaskContext"
+file_search_context_name = "FileSearchContext"
+file_mask_context_name = "FileMaskContext"
+
+
+def setup(session):
+
+    search_context = {
+        "name": search_context_name,
+        "matchers": [
+            {
+                "name": "SsnMatcher",
+                "type": "pattern",
+                "pattern": r"\b(\d{3}-\d{2}-\d{4})\b"
+            },
+            {
+                "name": "NameMatcher",
+                "type": "set",
+                "url": pathlib.Path('names.set').absolute().as_uri(),
+                "ignoreCase": True,
+                "matchWholeWords": True
+            },
+             {
+                "name": "EmailMatcher",
+                "type": "pattern",
+                "pattern": r"\b[\w._%+-]+@[\w.-]+\.[A-Za-z]{2,4}\b"
+            },
+             {
+                "name": "PhoneMatcher",
+                "type": "pattern",
+                "pattern": r"\b(\+?1?([ .-]?)?)?(\(?([2-9]\d{2})\)?([ .-]?)?)([2-9]\d{2})([ .-]?)(\d{4})(?: #?[eE][xX][tT]\.? \d{2,6})?\b"
+            }
+        ]
+    }
+
+    mask_context = {
+        "name": mask_context_name,
+        "rules": [
+            {
+                "name": "FpeRule",
+                "type": "cosort",
+                "expression": r"enc_fp_aes256_alphanum(${NAME})"
+            },
+            {
+                "name": "RedactSsnRule",
+                "type": "cosort",
+                "expression": r"replace_chars(${SSN},'*',1,3,'*',5,2)"
+            }
+        ],
+        "ruleMatchers": [
+            {
+                "name": "FpeRuleMatcher",
+                "type": "name",
+                "rule": "FpeRule",
+                "pattern": "EmailMatcher|NameMatcher|PhoneMatcher"
+            },
+            {
+                "name": "SsnRuleMatcher",
+                "type": "name",
+                "rule": "RedactSsnRule",
+                "pattern": "SsnMatcher"
+            }
+        ]
+    }
+
+    file_search_context = {
+        "name": file_search_context_name,
+        "matchers": [
+            {
+                "name": search_context_name,
+                "type": "searchContext"
+            }
+        ]
+    }
+
+    file_mask_context = {
+        "name": file_mask_context_name,
+        "rules": [
+            {
+                "name": mask_context_name,
+                "type": "maskContext"
+            }
+        ]
+    }
+
+    utils.create_context("searchContext", search_context, session)
+    utils.create_context("maskContext", mask_context, session)
+    utils.create_context("files/fileSearchContext", file_search_context, session)
+    utils.create_context("files/fileMaskContext", file_mask_context, session)
+
+
+def teardown(session):
+    utils.destroy_context("searchContext", search_context_name, session)
+    utils.destroy_context("maskContext", mask_context_name, session)
+    utils.destroy_context("files/fileSearchContext", file_search_context_name, session)
+    utils.destroy_context("files/fileMaskContext", file_mask_context_name, session)
